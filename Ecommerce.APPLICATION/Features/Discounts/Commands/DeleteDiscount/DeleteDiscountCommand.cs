4ebcd7e9f895e4a67f37f9e7d991885082ed0cb7
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2026-01-16 14:41:09 +0100

    Fix build errors in Discount commands and verify build

--- Ecommerce.APPLICATION/Features/Discounts/Commands/DeleteDiscount/DeleteDiscountCommand.cs
+++ Ecommerce.APPLICATION/Features/Discounts/Commands/DeleteDiscount/DeleteDiscountCommand.cs
@@ -1,6 +1,7 @@
 using Ecommerce.CORE.Constants;
 using Ecommerce.APPLICATION.Common.Security;
 using Ecommerce.APPLICATION.Common.Interfaces;
+using MediatR;
 
 namespace Ecommerce.APPLICATION.Features.Discounts.Commands.DeleteDiscount;
 
--- Ecommerce.APPLICATION/Features/Discounts/Commands/UpdateDiscount/UpdateDiscountCommand.cs
+++ Ecommerce.APPLICATION/Features/Discounts/Commands/UpdateDiscount/UpdateDiscountCommand.cs
@@ -2,6 +2,7 @@ using Ecommerce.CORE.Constants;
 using Ecommerce.CORE.Enums;
 using Ecommerce.APPLICATION.Common.Security;
 using Ecommerce.APPLICATION.Common.Interfaces;
+using MediatR;
 
 namespace Ecommerce.APPLICATION.Features.Discounts.Commands.UpdateDiscount;
 

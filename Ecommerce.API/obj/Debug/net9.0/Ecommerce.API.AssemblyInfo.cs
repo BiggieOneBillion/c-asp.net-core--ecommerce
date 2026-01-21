1db4aaa1cccbe27a326684e5425e42c6e80d2969
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2026-01-21 20:06:17 +0100

    added method to productRepo to get multiple products using their ids

--- Ecommerce.API/obj/Debug/net9.0/Ecommerce.API.AssemblyInfo.cs
+++ Ecommerce.API/obj/Debug/net9.0/Ecommerce.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8569a376cf17973ee0521f88d14108dd0a7565")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fd64f4cca1fbe77d97a5f84ec667acc8318157d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ecommerce.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

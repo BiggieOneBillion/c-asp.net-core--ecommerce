c3252d7629c95a7581a107d1541f12034bddb0f2
BiggieOneBillion <chukwuchinwendu94@gmail.com>
2026-02-22 22:17:39 +0100

    feat(application): add unified GeneralResponse model and update service interfaces

--- Ecommerce.APPLICATION/ResponseDTOs/GeneralResponse.cs
+++ Ecommerce.APPLICATION/ResponseDTOs/GeneralResponse.cs
@@ -1,9 +1,31 @@
 namespace Ecommerce.APPLICATION.ResponseDTOs;
 
-public class GeneralResponse <T>
+public class GeneralResponse<T>
 {
-    public T Data {get; set;}
+    public T? Data { get; set; }
     public bool Success { get; set; }
-    public string Message {get; set; }
+    public string Message { get; set; } = string.Empty;
     public int StatusCode { get; set; }
+
+    public static GeneralResponse<T> CreateSuccess(T? data, string message = "Success", int statusCode = 200)
+    {
+        return new GeneralResponse<T>
+        {
+            Data = data,
+            Success = true,
+            Message = message,
+            StatusCode = statusCode
+        };
+    }
+
+    public static GeneralResponse<T> CreateFailure(string message, int statusCode = 400)
+    {
+        return new GeneralResponse<T>
+        {
+            Data = default,
+            Success = false,
+            Message = message,
+            StatusCode = statusCode
+        };
+    }
 }
